2f0b02d17d2afdb2e905094281c3b718726121f8
tduncan14 <nbatre1@yahoo.com>
2024-07-07 15:30:57 -0700

    keep moving forward

---
+++ actions/stream.action.ts
@@ -0,0 +1,35 @@
+"use server"
+import { StreamClient } from "@stream-io/node-sdk";
+
+import { currentUser } from "@clerk/nextjs/server";
+
+const apiKey = process.env.NEXT_PUBLIC_STREAM_API_KEY;
+const apiSecret = process.env.STREAM_SECRET_KEY
+
+
+
+
+export const tokenProvider = async () => {
+    const user = await currentUser();
+
+    if (!user) throw new Error('User is not logged in')
+    if (!apiKey) throw new Error('No Api Key')
+    if (!apiSecret) throw new Error('No Api secret')
+
+
+    const client = new StreamClient(apiKey, apiSecret)
+
+    // expire date
+
+    const exp = Math.round(new Date().getTime() / 1000) + 60 * 60;
+
+    const issued = Math.floor(Date.now() / 1000) - 60
+
+
+    // creating the token
+
+
+    const token = client.createToken(user.id, exp, issued)
+
+    return token
+}
