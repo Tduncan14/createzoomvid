de2c902c41e9193e7099bd5f23a2006f48fee14d
tduncan14 <nbatre1@yahoo.com>
2024-07-18 11:08:57 -0700

    keep moving forward

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import Navbar from "@/components/Navbar";
 import { ClerkProvider, SignInButton, SignedIn, SignedOut, UserButton } from '@clerk/nextjs'
 import { Toaster } from "@/components/ui/toaster"
+import '@stream-io/video-react-sdk/dist/css/styles.css';
 
 const inter = Inter({ subsets: ["latin"] });
 
