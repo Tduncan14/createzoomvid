5e01d73e367a691cad885aae4ae52bb5103a0ce9
tduncan14 <nbatre1@yahoo.com>
2024-08-14 17:09:10 -0700

    keep moving forward

--- components/ui/EndCallButton.tsx
+++ components/ui/EndCallButton.tsx
@@ -1,16 +1,36 @@
 'use client'
 
-import { useCall } from '@stream-io/video-react-sdk'
+import { useCall, useCallStateHooks } from '@stream-io/video-react-sdk'
 import React from 'react'
+import { Button } from './button';
+import { useRouter } from 'next/navigation';
 
 const EndCallButton = () => {
 
     const call = useCall();
+    const router = useRouter()
+
+    const { useLocalParticipant } = useCallStateHooks()
+
+    const LocalParticipant = useLocalParticipant()
+
+    const isMeetingOwner = LocalParticipant && call?.state.createdBy && LocalParticipant.userId === call.state.createdBy.id;
+
+
+
+
+
+    if (!isMeetingOwner) return null
 
     return (
-        <div>
-            End Call
-        </div>
+        <Button className="bg-red-500" onClick={async () => {
+            await call.endCall();
+            router.push('/')
+        }}>
+
+            End call for everyone
+
+        </Button>
     )
 }
 
